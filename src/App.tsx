0fa013980c84af7da026b10453648159c0f185f8
Artun Aydın <artun.aydin@dijitalsahne.com>
2022-10-18 17:01:52 +0300

    Infinite Scroll add

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,55 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 import useSWR from "swr";
 import { Col, Row } from "react-bootstrap";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 function App() {
   const { data, error } = useSWR("https://api.publicapis.org/entries", fetcher);
-  const [viewData, setViewData] = useState(20);
+  const [viewData, setViewData] = useState(18);
 
   return (
-    <div className="container">
-      <Row>
+    <>
+      <div className="container">
         {data ? (
           <>
-            {data.entries.slice(0, viewData).map((c: any, i: number) => {
-              return (
-                <>
-                  <Col md={4}>
-                    <div>
-                      <p>{c.API}</p>
-                    </div>
-                  </Col>
-                </>
-              );
-            })}
+            <InfiniteScroll
+              className="list-container"
+              dataLength={data.entries.slice(0, viewData)} //This is important field to render the next data
+              next={() => setViewData(viewData + 20)}
+              hasMore={true}
+              loader={<></>}
+            >
+              <Row>
+                {data.entries.slice(0, viewData).map((c: any, i: number) => {
+                  return (
+                    <>
+                      <Col md={6} lg={4}>
+                        <div className="list-container__item">
+                          <p>{c.API}</p>
+                        </div>
+                      </Col>
+                    </>
+                  );
+                })}
+              </Row>
+            </InfiniteScroll>
           </>
         ) : (
           <></>
         )}
-      </Row>
 
-      <div
-        className="col-md-12 text-align"
-        onClick={() => setViewData(viewData + 20)}
-      >
-        Load More
+        <div
+          className="col-md-12 text-align"
+          onClick={() => setViewData(viewData + 20)}
+        >
+          Load More
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
